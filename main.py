93bb4e5575e3d741ec0425018de858fca9e4c66b
lunatic112 <70691573+lunatic112@users.noreply.github.com>
2022-08-24 02:26:30 +0200

    upd

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#training process
 from main_cat import cat_model as cat
 from main_bw import bw_model as bw
 from main_cy import cy_model as cy
+from main_bedroom import bedroom_model as bd
+from main_human_face import human_face_model as hf
+#baseline model
 import model_gp as WGANGP
 import model_dcgan as DCGAN
 
