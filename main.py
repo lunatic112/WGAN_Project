59b95a0a109f11695b06350e87399a0d84aa7bff
lunatic112 <70691573+lunatic112@users.noreply.github.com>
2022-08-24 02:17:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,4 @@ import model_dcgan as DCGAN
 Net1=bw(model=WGANGP)
 Net2=cy(model=WGANGP)
 Net3=cat(model=WGANGP)
-
-Net1.train()
-Net2.train(savepoint=[f'./savepoint/after_bw_G.pth', f'./savepoint/after_bw_D.pth'])
-Net3.train(savepoint=[f'./savepoint/after_cy_G.pth', f'./savepoint/after_cy_D.pth'])
+Net3.train(savepoint=Net2.train(savepoint=Net1.train()))
