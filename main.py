5076068fd60428ecb513b50dabc0546e0575c9dd
lunatic112 <70691573+lunatic112@users.noreply.github.com>
2022-08-27 15:07:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ Net5.train(Net4.train(Net3.train(Net2.train(Net1.train()))))
 '''
 #bed->cat->human->bw->cy
 model=DCGAN
-Net1=bd(model=model)
-Net2=cat(model=model)
+Net1=cy(model=model)
+Net2=bw(model=model)
 Net3=hf(model=model)
-Net4=bw(model=model)
-Net5=cy(model=model)
+Net4=cat(model=model)
+Net5=bd(model=model)
 Net5.train(Net4.train(Net3.train(Net2.train(Net1.train()))))
