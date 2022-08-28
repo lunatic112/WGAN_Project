3eef6da53b0375392473280023ed24141b15750c
lunatic112 <70691573+lunatic112@users.noreply.github.com>
2022-08-29 01:19:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from main_human_face import human_face_model as hf
 #baseline model
 import model_gp as WGANGP
 import model_dcgan as DCGAN
-'''
-#cy->bw->human->cat->bed-> DCGAN
-model=DCGAN
+
+#cy->bw->human->cat->bed-> WGANGP
+model=WGANGP
 Net1=cy(model=model)
 Net2=bw(model=model)
 Net3=hf(model=model)
@@ -25,3 +25,4 @@ Net3=hf(model=model)
 Net4=cat(model=model)
 Net5=bd(model=model)
 Net5.train(Net4.train(Net3.train(Net2.train(Net1.train()))))
+'''
